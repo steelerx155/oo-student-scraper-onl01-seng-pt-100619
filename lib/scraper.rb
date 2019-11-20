1f0488f9ce4fadc894e7f566b823fb1fbf332754
Randy Steele <steelerx155@gmail.com>
2019-11-20 18:13:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,16 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+  index_page =   Nokogiri::HTML(open(index_url))
+  index_page.css("div.roster-cards-container")
+  
   end
 
-  def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page(profile_url).each do |card|
+    card.css("div.student-card a")h do |student|
+      location = student.css(".student-location").text
+      profile-url = 
+      
     
   end
 
